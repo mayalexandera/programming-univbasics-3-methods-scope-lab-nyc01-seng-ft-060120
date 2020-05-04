d59f68baa58a9e0a32c4a0c4a19b3b1d31eec9e1
Maya Blevins <maya.blevins@gmail.com>
2020-05-04 17:55:08 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,25 @@
 
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  phrase = "It's-a me, Mario!"
+
 def mario
-  
+  phrase = "It's-a me, Mario!"
   puts phrase
+  puts danger
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+  puts danger
+end
+
+def danger 
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+def all_phrases(name)
+  if name === "mario"
+    return mario
+  else if name === 'toadstool'
+    return toadstool
+  end
 end
