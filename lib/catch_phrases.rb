1ea3c75f5586f1bdeaedef9daa9bd64865a048a1
Maya Blevins <maya.blevins@gmail.com>
2020-05-04 17:52:03 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,8 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
+def mario
+  
   puts phrase
 end
 
